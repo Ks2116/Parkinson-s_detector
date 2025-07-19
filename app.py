db011bfda8766ebdcea58c3cf63ce92147d8638e
Ks2116 <kaungsetpaing211002@gmail.com>
2025-07-20 05:38:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Parkinson's Clock Test", layout="centered")
 st.markdown("""
 <style>
 .stApp {
- background: linear-gradient(200deg, #1995ad, #a1d6e2, f1f1f2);
+ background: linear-gradient(200deg, #3B5BA5, #E87A5D, #F3B941);
  background-attachment: fixed;
     font-family: 'Segoe UI', sans-serif;
     color: #edf4f2;
