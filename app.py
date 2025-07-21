2e736d7af2a467a0b1284472a241d7c86b7c4111
Ks2116 <kaungsetpaing211002@gmail.com>
2025-07-21 16:08:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.markdown("""
 }
 
 html, body, .stApp {
-    background: linear-gradient(200deg, #eef6fa, #f8fbfe, #e9f3f7, #f6f9fc, #FFFFFF);
+    background: linear-gradient(200deg, #eef6fa, #f8fbfe, #e9f3f7, #f6f9fc, #FFFFFF, #F4F5F0);
     background-size: 300% 300%;
     background-attachment: fixed;
     animation: slowGradientShift 3s ease infinite;
